c5fe889972adb4efeb6ba45809ad545a624f0a22
Raffaele Montella <raffaele.montella@uniparthenope.it>
2016-07-29 18:28:42 +0000

    added protorype fromTile

--- lib/diles/dilefactory.py
+++ lib/diles/dilefactory.py
@@ -17,3 +17,9 @@ class dilefactory(object):
 	@staticmethod
 	def fromPoint(lon,lat,zoom=None):
 		pass
+
+        # Returns one or more diles as the z,x,y of a tile (http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/)
+        # It is used to know a dile list for a tile rendering
+        @staticmethod
+        def fromTile(z,x,y):
+                pass
