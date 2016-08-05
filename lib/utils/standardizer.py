889596856c31e54e1e7ca29075263a70528a76ef
Sergio <sergio.apreda@studenti.uniparthenope.it>
2016-08-04 22:41:22 -0700

    added the check for the lat/lon convention

--- lib/utils/standardizer.py
+++ lib/utils/standardizer.py
@@ -14,10 +14,12 @@ class standardizer(object):
 
 		self.__blacklist = []
 
-		self.__oldlat   = None # for rollback reasons
-		self.__oldlon   = None # for rollback reasons
-		self.__oldtime  = None # for rollback reasons
-		self.__oldlev   = None # for rollback reasons
+		self.__oldlat    = None  # for rollback reasons
+		self.__oldlon    = None  # for rollback reasons
+		self.__oldtime   = None  # for rollback reasons
+		self.__oldlev 	 = None  # for rollback reasons
+		self.__latscaled = False # for rollback reasons
+		self.__lonscaled = False # for rollback reasons
 
 
 
@@ -30,8 +32,8 @@ class standardizer(object):
 		except:
 			print path+file," file couldn't be opened"
 
-# rollback the changes to the dimensions' names
-	def __rollback(self):
+#	rollback the changes to the dimensions' names
+	def __rollbackNames(self):
 
 		if self.__oldlat is not None:
 			self.__rgrp.renameDimension('lat',self.__oldlat)
@@ -49,23 +51,30 @@ class standardizer(object):
 			self.__rgrp.renameDimension('level',self.__oldlevel)
 			self.__rgrp.renameVariable('level',self.__oldlevel)
 
-#	main function
-	def createNetcdf(self):
 
-		ncp  = NetcdfCopier()
-		dst  = None
+	def __rollbackGrid(self):
 
-		self.__netcdfOpen(self.__srcpath,self.__file,self.__mode)
+		if self.__latscaled:
+			self.__rgrp.variables["lat"][:] += 90
+
+		if self.__lonscaled:
+			self.__rgrp.variables["lon"][:] += 180
+
+	
+# 	try to set the range of lat/lon properly if the values are 
+#	in the range lat->[0:90], lon->[0:180] the result is ambiguous
+	def __checkRange(self):
 		
-		geo = self.__checkUnits()
-		if geo:	
-				srcpath = self.__srcpath+self.__file
-				dstpath = self.__dstpath+self.__file
-				dst = ncp.copy(srcpath,dstpath,dbl=self.__blacklist)
-		
-		self.__rollback()
+		if max(self.__rgrp.variables["lon"]) > 180:
+			self.__rgrp.variables["lon"][:] -= 180
+			self.__lonscaled = True
 
-		return dst
+		if max(self.__rgrp.variables["lat"]) > 90:
+			
+			self.__rgrp.variables["lat"][:] -= 90
+			self.__latscaled = True
+
+
 
 #	set the convention for the dimentions involved
 	def __checkUnits(self):
@@ -126,6 +135,29 @@ class standardizer(object):
 				self.__blacklist.append(dimension.name)		# if the dimension is has not a variable describing it
 		
 		return lat and lon
+
+
+#	main function
+	def createNetcdf(self):
+
+		ncp  = NetcdfCopier()
+		dst  = None
+
+		self.__netcdfOpen(self.__srcpath,self.__file,self.__mode)
+		
+		geo = self.__checkUnits()
+		if geo:
+				self.__checkRange()
+				
+				srcpath = self.__srcpath+self.__file
+				dstpath = self.__dstpath+self.__file
+				dst = ncp.copy(srcpath,dstpath,dbl=self.__blacklist)
+				
+				self.__rollbackGrid()
+		
+		self.__rollbackNames()
+
+		return dst
 
 
 if __name__ == '__main__':
