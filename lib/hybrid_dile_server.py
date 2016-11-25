6b6d7d0954dd108926f8ed89b7a8022441904f44
Sergio <sergio.apreda@studenti.uniparthenope.it>
2016-11-25 04:29:38 -0800

    sync

--- lib/hybrid_dile_server.py
+++ lib/hybrid_dile_server.py
@@ -602,6 +602,7 @@ def select_dile_by_uri():
                         key.open_read()                         # opens the file
                         headers = dict(key.resp.getheaders())   # request the headers
                         headers["filename"] = pathLeaf(key.name)
+                        headers["Content-Disposition"] = "inline"
                         return Response(key, headers=headers)   # return a response                                  
                     except S3ResponseError as e:
                         return Response(e.body, status=e.status, headers=key.resp.getheaders())            
