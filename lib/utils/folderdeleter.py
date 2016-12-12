4b5b09024c874252024f44e665b5eb3fd8b3f771
Sergio <sergio.apreda@studenti.uniparthenope.it>
2016-12-12 07:19:07 -0800

    to delete folder in s3

---
+++ lib/utils/folderdeleter.py
@@ -0,0 +1,34 @@
+import boto
+import boto.s3
+from   misc import printProgress, pathLeaf
+
+idkeypath 		= '/home/sergio/.s3/AWS_ACCESS_KEY_ID'
+secretkeypath 	= '/home/sergio/.s3/AWS_SECRET_ACCESS_KEY'
+bucketname 		= 'edu-uchicago-rdcep-diles'
+folder          = ''
+
+with open(idkeypath) as myfile:
+	id_access_key = myfile.read().replace('\n', '')
+
+with open(secretkeypath) as myfile:
+	secret_access_key = myfile.read().replace('\n', '')
+
+print "connecting to s3"
+try:
+	conn =  boto.connect_s3(id_access_key,secret_access_key)
+except:
+	print "failed to connect!"
+	exit()
+else:
+	print "connected succesfully"
+
+bucket = conn.get_bucket(bucketname)
+
+
+keys = bucket.list(prefix = folder)
+
+i = 0
+for key in keys:
+	key.delete()
+	i = i+1
+	print i, "\r"
